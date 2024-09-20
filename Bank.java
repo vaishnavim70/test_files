5b6366c714e42904829e4d132a9e6bfdf99854d7
vaishnavim70 <140974561+vaishnavim70@users.noreply.github.com>
2024-09-20 11:56:54 +0530

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,29 @@
+package SetA;
+
+import java.util.*;
+
+public class Bank {
+
+	
+	public static void main(String[] args) {
+		
+		Scanner Sc = new Scanner(System.in);
+		System.out.println("Enter the amount");
+		int i = Sc.nextInt();
+		
+		
+		for(i=0;i<=0;i++) {
+			
+		if(i<=0) {
+			
+			System.out.println("print amount");
+		}
+		else {
+			System.out.println("custom error");
+			
+		}		
+		
+	}
+
+  }
+}
---
+++ Program2.java
@@ -0,0 +1,31 @@
+package SetA;
+
+import java.util.Scanner;
+
+public class Program2 {
+	
+public static void main(String[] args) {
+		
+		//int i = 28;
+		//int j = 13;
+		
+		Scanner Sc = new Scanner(System.in);
+		System.out.println("Write here: ");
+		int i = Sc.nextInt();
+		int j = 28;
+		
+		for(i=0;i<=j;i++)
+		{
+			if(i<=j) {
+				System.out.println("2,4,7,14");
+			}
+			else {
+				System.out.println("Error");
+			}
+			
+		
+		}	
+		
+	}
+
+}
